ad3884f2471312db57326e900f5e3df103f0e271
starjara <starjara@pusan.ac.kr>
2024-08-22 02:37:04 +0900

    Now using VMID in HGATP

--- arch/riscv/verse/vmid.c
+++ arch/riscv/verse/vmid.c
@@ -2,6 +2,7 @@
 #include <linux/verse_host.h>
 
 static unsigned long vmid_bits __ro_after_init;
+static unsigned long vmidindex = 0;
 
 unsigned long verse_riscv_gstage_vmid_bits(void)
 {
@@ -11,7 +12,7 @@ unsigned long verse_riscv_gstage_vmid_bits(void)
 int verse_riscv_gstage_vmid_init(struct verse *verse)
 {
   verse->arch.vmid.vmid_version = 0;
-  verse->arch.vmid.vmid = 0;
+  verse->arch.vmid.vmid = vmidindex ++;
 
   return 0;
 }
